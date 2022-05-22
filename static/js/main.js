ea142430accb0566b83eee01908e26aa6ad78c1f
Maciej Pawlica <maciejpawlica@MacBook-Air-Maciej.local>
2022-05-22 18:51:37 +0200

    added news apartaments section, adjusted it to all breakpoints. also added an .svg in header and fixed mobile navigation

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,18 @@
 const navMobile = document.querySelector('.nav-mobile-items')
 const burgerIcon = document.querySelector('.hamburger')
+const navMobileItems = document.querySelectorAll('.nav-mobile-item')
 
 //FUNCTION THAT HANDLES MOBILE NAVIGATION
 const handleNav = () => {
     navMobile.classList.toggle('active')
     burgerIcon.classList.toggle('is-active')
+
+    navMobileItems.forEach(item => {
+        item.addEventListener('click', () => {
+            navMobile.classList.remove('active')
+            burgerIcon.classList.remove('is-active')
+        })
+    })
 }
 
 burgerIcon.addEventListener('click', handleNav)
