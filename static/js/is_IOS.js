04f1d409a1e62ae51a57036497984d465276179e
Maciej Pawlica <maciejpawlica@MacBook-Air-Maciej.local>
2022-05-30 10:54:51 +0200

    added script for background attachment in IOS

---
+++ static/js/is_IOS.js
@@ -0,0 +1,19 @@
+//script that changes bacground-attachment to scroll on iOS devices
+function iOS() {
+    return [
+            'iPad Simulator',
+            'iPhone Simulator',
+            'iPod Simulator',
+            'iPad',
+            'iPhone',
+            'iPod'
+        ].includes(navigator.platform)
+        // iPad on iOS 13 detection
+        ||
+        (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+}
+
+if (iOS() == true) {
+    document.getElementById("apartament-details").style.backgroundAttachment = "scroll";
+    document.getElementById("contact").style.backgroundAttachment = "scroll";
+}
